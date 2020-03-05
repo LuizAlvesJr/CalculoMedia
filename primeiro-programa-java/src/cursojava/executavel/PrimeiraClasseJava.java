dc5c1771cea936f068a8faaedf79cfc0dadbef2e
Treinamento <Treinamento@Lenovo-PC>
2020-03-05 18:09:17 -0300

    Primeiro Commit

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,37 @@
+package cursojava.executavel;
+
+import javax.swing.JOptionPane;
+
+public class PrimeiraClasseJava {
+
+	public static void main(String[] args) {
+			
+			
+		String nota1 = JOptionPane.showInputDialog("Informe nota 1: ");
+		String nota2 = JOptionPane.showInputDialog("Informe nota 2: ");
+		String nota3 = JOptionPane.showInputDialog("Informe nota 3: ");
+		String nota4 = JOptionPane.showInputDialog("Informe nota 4: ");
+		
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+
+		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+		
+		if (media >= 50) {
+			
+			if(media >= 70 ) {
+				
+				JOptionPane.showMessageDialog(null, "Aluno está aprovado com média: "+ media);
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "Aluno em recupeção com média: "+ media);
+			}
+		}else {
+			JOptionPane.showMessageDialog(null, "Aluno está reprovado com média:"+ media);
+		}
+		
+	}
+
+}
